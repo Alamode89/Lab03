b4d87c9d7a9d767ed9318235790c076e3a4f4a2e
Alamode89 <andreamante89@gmail.com>
2021-01-26 19:29:03 -0800

    Added the Add class and add_test.hpp. Updated unit_test.cpp.

---
+++ add.hpp
@@ -0,0 +1,30 @@
+#ifndef __ADD_HPP__
+#define __ADD_HPP__
+
+#include "op.hpp"
+using namespace std;
+
+class Add : public Base {
+	public:
+		Add(Base* left, Base* right) : Base() {
+			leftVal = left->evaluate();
+			rightVal = right->evaluate();
+			leftStr = left->stringify();
+			rightStr = right->stringify();
+		}
+		
+		double evaluate() {
+			return (leftVal + rightVal);
+		}
+		
+		string stringify() {
+			return leftStr + " + " + rightStr;
+		}
+
+	private:
+		double leftVal;
+		double rightVal;
+		string leftStr;
+		string rightStr;
+};
+#endif
